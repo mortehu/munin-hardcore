83fadf0a5510f20fb1dedb8aa87d847d44bd8a21
Morten Hustveit <morten@rashbox.org>
2009-08-01 12:25:05 +0200

    Use FreeType's built-in glyph cache.

--- font.c
+++ font.c
@@ -4,13 +4,28 @@
 
 #include <ft2build.h>
 #include FT_FREETYPE_H
+#include FT_CACHE_H
 
 #include "types.h"
 
-FT_Library ft_library;
-FT_Face ft_face;
+FT_Library       ft_library;
+FT_Face          ft_face;
+FTC_Manager      ft_cache_mgr;
+FTC_CMapCache    ft_cmap_cache;
+FTC_SBitCache    ft_sbit_cache;
+FTC_ImageTypeRec ft_image_type;
+
 
 #define FONT_NAME "/usr/share/munin/VeraMono.ttf"
+#define FONT_CACHE_SIZE (1024 * 1024)
+
+static FT_Error
+face_requester(FTC_FaceID face_id, FT_Library library, FT_Pointer request_data, FT_Face* aface)
+{
+  *aface = ft_face;
+
+  return 0;
+}
 
 void
 font_init()
@@ -22,15 +37,25 @@ font_init()
   if(result)
     errx(EXIT_FAILURE, "Error initializing FreeType: %d", result);
 
-  result = FT_New_Face(ft_library, FONT_NAME, 0, &ft_face);
-
-  if(result)
+  if(0 != (result = FT_New_Face(ft_library, FONT_NAME, 0, &ft_face)))
     errx(EXIT_FAILURE, "Error opening font '%s': %d", FONT_NAME, result);
 
-  result = FT_Set_Pixel_Sizes(ft_face, 0, 10);
-
-  if(result)
+  if(0 != (result = FT_Set_Pixel_Sizes(ft_face, 0, 10)))
     errx(EXIT_FAILURE, "Error setting size (%d pixels): %d", 10, result);
+
+  if(0 != (result = FTC_Manager_New(ft_library, 1, 1, FONT_CACHE_SIZE, face_requester, NULL, &ft_cache_mgr)))
+    errx(EXIT_FAILURE, "Failed to create font cache manager: %d", result);
+
+  if(0 != (result = FTC_CMapCache_New(ft_cache_mgr, &ft_cmap_cache)))
+    errx(EXIT_FAILURE, "Failed to create font character map cache: %d", result);
+
+  if(0 != (result = FTC_SBitCache_New(ft_cache_mgr, &ft_sbit_cache)))
+    errx(EXIT_FAILURE, "Failed to create font image cache: %d", result);
+
+  memset(&ft_image_type, 0, sizeof(ft_image_type));
+  ft_image_type.width = 10;
+  ft_image_type.height = 10;
+  ft_image_type.flags = FT_LOAD_DEFAULT | FT_LOAD_RENDER;
 }
 
 size_t
@@ -39,7 +64,7 @@ font_width(const char* text)
   size_t width = 0;
   int result;
   FT_UInt idx;
-  FT_GlyphSlot slot;
+  FTC_SBit sbit;
 
   while(*text)
   {
@@ -61,19 +86,15 @@ font_width(const char* text)
     while(n-- && *text)
       ch <<= 6, ch |= (*text++ & 0x3F);
 
-    idx = FT_Get_Char_Index(ft_face, ch);
+    idx = FTC_CMapCache_Lookup(ft_cmap_cache, 0, 0, ch);
 
     if(!idx)
       continue;
 
-    result = FT_Load_Glyph(ft_face, idx, FT_LOAD_NO_BITMAP);
-
-    if(result)
+    if(0 != (result = FTC_SBitCache_Lookup(ft_sbit_cache, &ft_image_type, idx, &sbit, 0)))
       continue;
 
-    slot = ft_face->glyph;
-
-    width += slot->advance.x >> 6;
+    width += sbit->xadvance;
   }
 
   return width;
@@ -84,7 +105,7 @@ font_draw(struct canvas* canvas, size_t x, size_t y, const char* text, int direc
 {
   int result;
   FT_UInt idx;
-  FT_GlyphSlot slot;
+  FTC_SBit sbit;
   size_t yy, xx;
 
   if(direction == -1)
@@ -112,20 +133,16 @@ font_draw(struct canvas* canvas, size_t x, size_t y, const char* text, int direc
     while(n-- && *text)
       ch <<= 6, ch |= (*text++ & 0x3F);
 
-    idx = FT_Get_Char_Index(ft_face, ch);
+    idx = FTC_CMapCache_Lookup(ft_cmap_cache, 0, 0, ch);
 
     if(!idx)
       continue;
 
-    result = FT_Load_Glyph(ft_face, idx, FT_LOAD_RENDER);
-
-    if(result)
+    if(0 != (result = FTC_SBitCache_Lookup(ft_sbit_cache, &ft_image_type, idx, &sbit, 0)))
       continue;
 
-    slot = ft_face->glyph;
-
-    int x_off = slot->metrics.horiBearingX >> 6;
-    int y_off = -(slot->metrics.horiBearingY >> 6) + (ft_face->descender >> 8) - 1;
+    int x_off = sbit->left;
+    int y_off = -sbit->top + (ft_face->descender >> 8) - 1;
 
     switch(direction)
     {
@@ -133,14 +150,14 @@ font_draw(struct canvas* canvas, size_t x, size_t y, const char* text, int direc
     case -1:
     case 0:
 
-      for(yy = 0; yy < slot->bitmap.rows; ++yy)
+      for(yy = 0; yy < sbit->height; ++yy)
       {
         int eff_y = (y + yy + y_off);
 
         if(eff_y < 0 || eff_y >= canvas->height)
           continue;
 
-        for(xx = 0; xx < slot->bitmap.width; ++xx)
+        for(xx = 0; xx < sbit->width; ++xx)
         {
           int eff_x = (x + xx + x_off);
 
@@ -149,7 +166,7 @@ font_draw(struct canvas* canvas, size_t x, size_t y, const char* text, int direc
 
           size_t i = (eff_y * canvas->width + eff_x) * 3;
 
-          unsigned int alpha = slot->bitmap.buffer[yy * slot->bitmap.width + xx];
+          unsigned int alpha = sbit->buffer[yy * sbit->pitch + xx];
           unsigned int inv_alpha = 256 - alpha;
 
           canvas->data[i + 0] = (canvas->data[i + 0] * inv_alpha) >> 8;
@@ -158,20 +175,20 @@ font_draw(struct canvas* canvas, size_t x, size_t y, const char* text, int direc
         }
       }
 
-      x += slot->advance.x >> 6;
+      x += sbit->xadvance;
 
       break;
 
     case 1:
 
-      for(yy = 0; yy < slot->bitmap.rows; ++yy)
+      for(yy = 0; yy < sbit->height; ++yy)
       {
         int eff_x = (x - yy - y_off);
 
         if(eff_x < 0 || eff_x >= canvas->width)
           continue;
 
-        for(xx = 0; xx < slot->bitmap.width; ++xx)
+        for(xx = 0; xx < sbit->width; ++xx)
         {
           int eff_y = (y + xx + x_off);
 
@@ -181,7 +198,7 @@ font_draw(struct canvas* canvas, size_t x, size_t y, const char* text, int direc
 
           size_t i = (eff_y * canvas->width + eff_x) * 3;
 
-          unsigned int alpha = slot->bitmap.buffer[yy * slot->bitmap.width + xx];
+          unsigned int alpha = sbit->buffer[yy * sbit->pitch + xx];
           unsigned int inv_alpha = 256 - alpha;
 
           canvas->data[i + 0] = (canvas->data[i + 0] * inv_alpha) >> 8;
@@ -190,20 +207,20 @@ font_draw(struct canvas* canvas, size_t x, size_t y, const char* text, int direc
         }
       }
 
-      y += slot->advance.x >> 6;
+      y += sbit->xadvance;
 
       break;
 
     case 2:
 
-      for(yy = 0; yy < slot->bitmap.rows; ++yy)
+      for(yy = 0; yy < sbit->height; ++yy)
       {
         int eff_x = (x + yy + y_off);
 
         if(eff_x < 0 || eff_x >= canvas->width)
           continue;
 
-        for(xx = 0; xx < slot->bitmap.width; ++xx)
+        for(xx = 0; xx < sbit->width; ++xx)
         {
           int eff_y = (y - xx - x_off);
 
@@ -212,7 +229,7 @@ font_draw(struct canvas* canvas, size_t x, size_t y, const char* text, int direc
 
           size_t i = (eff_y * canvas->width + eff_x) * 3;
 
-          unsigned int alpha = slot->bitmap.buffer[yy * slot->bitmap.width + xx];
+          unsigned int alpha = sbit->buffer[yy * sbit->pitch + xx];
           unsigned int inv_alpha = 256 - alpha;
 
           canvas->data[i + 0] = (canvas->data[i + 0] * inv_alpha) >> 8;
@@ -221,7 +238,7 @@ font_draw(struct canvas* canvas, size_t x, size_t y, const char* text, int direc
         }
       }
 
-      y -= slot->advance.x >> 6;
+      y -= sbit->xadvance;
 
       break;
     }
